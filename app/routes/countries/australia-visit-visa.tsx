6f1d27ccbcd949cdde5befa71e49e9766da7d4e3
M. Abdullah Butt <abutt2210@gmail.com>
2025-04-27 20:08:37 +0500

    Add Australia Visit Visa page with meta information for SEO

---
+++ app/routes/countries/australia-visit-visa.tsx
@@ -0,0 +1,14 @@
+import AustraliaVisitVisa from "~/components/CountryDetail/AustraliaVisitVisa";
+
+export default function AuVisitVisaPage() {
+  return (
+    <AustraliaVisitVisa />
+  );
+}
+
+export function meta() {
+  return [
+    { title: "Australia Visit Visa - GQ Consultants" },
+    { name: "description", content: "Information about Australia visit visa requirements, application process, and documents needed for Pakistani applicants." },
+  ];
+};
