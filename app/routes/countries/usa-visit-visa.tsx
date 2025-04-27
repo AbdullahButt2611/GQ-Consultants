a65c3058c731cea41422dfbfa630109313a3c594
M. Abdullah Butt <abutt2210@gmail.com>
2025-04-27 20:23:59 +0500

    Add USA Visit Visa page with detailed information and SEO meta tags

---
+++ app/routes/countries/usa-visit-visa.tsx
@@ -0,0 +1,14 @@
+import USVisitVisa from "~/components/CountryDetail/USVisitVisa";
+
+export default function USAVisitVisaPage() {
+  return (
+    <USVisitVisa />
+  );
+}
+
+export function meta() {
+  return [
+    { title: "USA Visit Visa - GQ Consultants" },
+    { name: "description", content: "Information about USA visit visa requirements, application process, and documents needed for Pakistani applicants." },
+  ];
+};
