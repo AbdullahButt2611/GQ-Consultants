f4e54cd02d5a82e40347c89d5faeab20db23136d
M. Abdullah Butt <abutt2210@gmail.com>
2025-04-27 19:52:58 +0500

    Add UK Visit Visa route and meta information

---
+++ app/routes/countries/uk-visit-visa.tsx
@@ -0,0 +1,14 @@
+import UKVisitVisa from "~/components/CountryDetail/UKVisitVisa";
+
+export default function UnitedKingdomVisitVisa() {
+  return (
+    <UKVisitVisa />
+  );
+}
+
+export function meta() {
+    return [
+      { title: "UK Visit Visa - GQ Consultants" },
+      { name: "description", content: "Information about UK visit visa requirements, application process, and documents needed for Pakistani applicants." },
+    ];
+  };
