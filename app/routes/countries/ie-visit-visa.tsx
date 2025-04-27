94ac90a55167bfbab511f0252647b726614eb989
M. Abdullah Butt <abutt2210@gmail.com>
2025-04-27 20:52:21 +0500

    Add Ireland Visit Visa page with meta information and component integration

---
+++ app/routes/countries/ie-visit-visa.tsx
@@ -0,0 +1,14 @@
+import IrelandVisitVisa from "~/components/CountryDetail/IrelandVisitVisa";
+
+export default function IrelandVisitVisaPage() {
+  return (
+    <IrelandVisitVisa />
+  );
+}
+
+export function meta() {
+  return [
+    { title: "Ireland Visit Visa - GQ Consultants" },
+    { name: "description", content: "Information about Ireland visit visa requirements, application process, and documents needed for Pakistani applicants." },
+  ];
+};
