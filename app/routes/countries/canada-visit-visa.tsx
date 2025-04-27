c4e3afeb73b341fedf3a8199fadb0a43d5147240
M. Abdullah Butt <abutt2210@gmail.com>
2025-04-27 19:35:35 +0500

    Add Canada Visit Visa route and meta information

---
+++ app/routes/countries/canada-visit-visa.tsx
@@ -0,0 +1,15 @@
+import CAVisitVisa from "~/components/CountryDetail/CanadaVisitVisa";
+
+export default function CanadaVisitVisa() {
+  return <CAVisitVisa />;
+}
+
+export function meta() {
+  return [
+    { title: "Canada Visit Visa Information | GQ Consultants" },
+    { 
+      name: "description", 
+      content: "Complete guide to Canada visit visa application process, requirements and documentation - GQ Consultants" 
+    },
+  ];
+}
